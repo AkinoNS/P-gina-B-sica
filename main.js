3c9df4b5a43135b321a93deb80087129681de350
David <juanda.v.1994@gmail.com>
2024-08-30 23:09:47 -0500

    Mi Primer Commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const diasDeLaSemana = [ "Lunes", "Martes", "Miercoles", ];
+
+console.log(diasDeLaSemana);
+
+diasDeLaSemana.push("Jueves");
+diasDeLaSemana.unshift("Domingo");
+
+diasDeLaSemana.pop("Miercoles");
+diasDeLaSemana.shift("Lunes");
+
+console.log(diasDeLaSemana);
+
+
+let i = 0;
+while(i < 10){
+    console.log(i);
+
+    i++
+}
